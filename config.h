8ce1bf9d4daff68b3496034f3eff9cd64d11a525
Abel DV <mail@abel.fyi>
2026-09-19 03:02:03 +0200

    Use generic bar color name

--- config.h
+++ config.h
@@ -10,16 +10,16 @@ static const char *fonts[]          = { "monospace:size=13" };
 static const char dmenufont[]       = "monospace:size=13";
 static unsigned int baralpha        = 0x60;
 static unsigned int borderalpha     = OPAQUE;
-static const char col_gray1[]       = "#222222";
+static const char col_black[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_bar[]         = "#005577";
 static const char col_red[]         = "#cc0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_red   },
+	[SchemeNorm] = { col_gray3, col_black, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_bar,   col_red   },
 };
 
 /* tagging */
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-p", "🚀 Run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "🚀 Run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray3, "-sb", col_bar, "-sf", col_gray4, NULL };
 static const char *emojicmd[] = { "/bin/sh", "-c",
 	"curl -fsSL https://www.unicode.org/Public/emoji/latest/emoji-test.txt | "
 	"awk -F'# ' '/; fully-qualified/ { sub(/ E[0-9.]+ /, \" \", $2); print $2 }' | "
